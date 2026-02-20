96111353137ef8f3fe98b66d0da20d66ea040549
jonel-ascoitia <1618942@senati.pe>
2026-02-20 14:16:44 -0500

    Fix Vercel 500 error configuration

--- api/server.js
+++ api/server.js
@@ -2,11 +2,11 @@ import { buildApp } from '../src/app.js';
 
 let appInstance = null;
 
-export default async (req, res) => {
+export default async function handler(req, res) {
     if (!appInstance) {
         appInstance = await buildApp();
         await appInstance.ready();
     }
 
     appInstance.server.emit('request', req, res);
-};
+}
